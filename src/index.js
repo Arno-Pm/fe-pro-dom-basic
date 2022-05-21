50f5bbc0d6212a150056649b000f8f25e2e626ef
Arno-Pm <70451630+Arno-Pm@users.noreply.github.com>
2022-05-22 00:21:27 +0300

    add work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+    const odd = document.querySelectorAll('li:nth-child(odd)');// тут еще можно .item вместо li , работает и так и так
+  for (let element of odd) {
+    element.style.backgroundColor = 'red';
+  };
+};
 
-export const findElement = () => {};
+
+export const findElement = () => {
+    const FirstStep = document.querySelectorAll('li');
+    let NumberOneLi = document.querySelector('ul > li:first-child');
+    for (let i = 0; i < FirstStep.length; i++) {
+        let nextLI = NumberOneLi.nextElementSibling;
+        if (nextLI.matches('.item.likedItem')) {
+            nextLI.style.backgroundColor = 'blue';
+          } else {
+            NumberOneLi = nextLI;
+          }
+        }
+      };
